e99c48f71a997d5f97907ed39a984ce48b39ae08
Taukir397 <74283105+Taukir397@users.noreply.github.com>
2021-09-07 22:52:15 +0600

    Create fusion.py

---
+++ fusion.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import pandas as pd
+import numpy as np
+data = pd.read_csv("~/msisdn/csv/fusion.csv",
+                             sep="\t",
+                             header=None,
+                             names=["Duration","Aparty","Operator's Name","Count"])
+count = data.groupby(['Aparty'])['Count'].count().reset_index()
+sum = data.groupby(['Aparty'])['Duration'].sum().reset_index()
+ac = pd.merge(count, sum, on='Aparty', how='inner')
+dfn = data [["Aparty", "Operator's Name"]]
+df = dfn.drop_duplicates(subset=['Aparty'])
+acd = pd.merge(df, ac, on='Aparty', how='inner')
+acd['ACD'] = acd['Duration']/acd['Count']
+final = acd[(acd['Duration']>=300000) & (acd['ACD']>24000)][['Aparty',"Operator's Name",'Count','Duration','ACD']]
+final['ACD'] = final['ACD']/6000
+final['Duration'] = final['Duration']/6000
+final.to_csv(r'~/msisdn/csv/taukir_goodness_fusion.csv', index = False)
