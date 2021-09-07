15d0b69a7cccd63691136e9455ba2281a5a3f23c
Taukir397 <74283105+Taukir397@users.noreply.github.com>
2021-09-07 22:59:28 +0600

    Create mail.sh

---
+++ mail.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sendemail -f agni@example.com -t taukir397@gmail.com  -m "Curtesy by taukir" -u "Operator wise MSISDN list & total call duration≥50 Minutes & ACD≥4 minutes" -a ~/msisdn/csv/taukir_msisdn_btrc.csv
